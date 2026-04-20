42a2c8de41ad20fdf7a326812d36bacf086b8c1a
Kellan Cerveny <kjcerveny@gmail.com>
2026-04-20 18:00:53 -0500

    Secret error throwing, logging enabled

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,6 +4,12 @@ Sentry.init({
   dsn: import.meta.env.PUBLIC_SENTRY_DSN,
   environment: import.meta.env.MODE,
   tunnel: import.meta.env.PROD ? "/.netlify/functions/monitoring" : undefined,
+  integrations: [
+    // send console.log, console.warn, and console.error calls as logs to Sentry
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
+  // Logs can get sent to sentry
+  enableLogs: true,
   tracesSampleRate: 0,
   replaysSessionSampleRate: 0,
   replaysOnErrorSampleRate: 0,
