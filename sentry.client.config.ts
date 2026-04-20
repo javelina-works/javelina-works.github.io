fc882e8fa11b0b87ff7d5319ad84d9d01dcb4609
Kellan Cerveny <kjcerveny@gmail.com>
2026-04-20 17:15:12 -0500

    Add sentry tunneling

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,6 +3,7 @@ import * as Sentry from "@sentry/astro";
 Sentry.init({
   dsn: import.meta.env.PUBLIC_SENTRY_DSN,
   environment: import.meta.env.MODE,
+  tunnel: import.meta.env.PROD ? "/.netlify/functions/monitoring" : undefined,
   tracesSampleRate: 0,
   replaysSessionSampleRate: 0,
   replaysOnErrorSampleRate: 0,
